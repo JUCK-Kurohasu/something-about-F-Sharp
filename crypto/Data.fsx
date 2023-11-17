5a5591f06d8b9387f4e044b86303147cca20a875
K-Kurohasu <137016169+JUCK-Kurohasu@users.noreply.github.com>
2023-11-17 04:04:56 +0000

    add data.fsx

---
+++ crypto/Data.fsx
@@ -0,0 +1,52 @@
+open System
+open System.Text
+open System.Net.Security
+open System.Net.Http
+open System.Security.Cryptography
+open System.Text.Encodings
+
+open System.Text.Json
+open System.IO
+
+#r "nuget: FSharp.Data "
+//dotnet add package FSharp.Data --version 6.3.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
