0a998e2e9aa6ba8c062f203146ce3c8e3d9c4474
K-Kurohasu <137016169+JUCK-Kurohasu@users.noreply.github.com>
2023-11-18 08:49:38 +0000

    add

--- Data/Program.fs
+++ Data/Program.fs
@@ -44,17 +44,28 @@ printfn "%s" str
 //str<-Http.RequestString("https://chainflyer.bitflyer.com/v1/block/000000006a625f06636b8bb6ac7b960a8d03705d1ace08b1a19da3fdcc99ddbd")
 //printfn "%s" str
 
-let ret=JsonValue.Load("https://chainflyer.bitflyer.com/v1/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
+let data=JsonValue.Load("https://chainflyer.bitflyer.com/v1/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
 //printfn "%A" ret
 //let J=JsonValue.WriteTo()
 
-let p=JsonValue.Parse(ret)
-//printfn "%s" p
+let p=data?bits
+printfn "bit:%s" (p.AsString())
 
-let atai=p?height
-printfn "%A" atai
-
-
+//let atai=p?height
+//printfn "%A" atai
 
 
+let export:block={
+    block_hash=(data?block_hash.AsString())
+    height=(data?height.AsInteger64())|>uint64
+    is_main=(data?is_main.AsBoolean())
+    version=(data?version.AsInteger64())|>uint64
+    prev_block=(data?prev_block.AsString())
+    merkle_root=(data?merkle_root.AsString())
+    timestamp=(data?timestamp.AsDateTime())
+    bits=(data?bits.AsInteger64())|>uint64
+    nonce=(data?nonce.AsInteger64())|>uint64
+    txnum=(data?)
+}
 
+printfn "%A" export
